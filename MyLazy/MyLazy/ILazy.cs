952cb15dfbf045a1649a576235bf374d972a3626
SaveliyLipaev <sava.lipaev@mail.ru>
2019-09-21 16:34:45 +0300

    Comments added

--- MyLazy/MyLazy/ILazy.cs
+++ MyLazy/MyLazy/ILazy.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace MyLazy
 {
+    /// <summary>
+    /// Интерфейс, представляющий ленивое вычисление
+    /// </summary>
     public interface ILazy<T>
     {
         T Get();
--- MyLazy/MyLazy/LazyFactory.cs
+++ MyLazy/MyLazy/LazyFactory.cs
@@ -4,14 +4,24 @@ using System.Text;
 
 namespace MyLazy
 {
+    /// <summary>
+    /// Класс, возвращающий две разные реализации ILazy<T>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public static class LazyFactory<T>
     {
-        public static MySimpleLazy<T> CreateSimpleLazy<T>(Func<T> supplier)
+        /// <summary>
+        /// Возвращает простоую реализацию Lazy
+        /// </summary>
+        public static MySimpleLazy<T> CreateSimpleLazy(Func<T> supplier)
         {
             return new MySimpleLazy<T>(supplier);
         }
 
-        public static MyMultithreadedLazy<T> CreateMymultithreadedLazy<T>(Func<T> supplier)
+        /// <summary>
+        /// Возвращает многопоточную реализацию Lazy
+        /// </summary>
+        public static MyMultithreadedLazy<T> CreateMymultithreadedLazy(Func<T> supplier)
         {
             return new MyMultithreadedLazy<T>(supplier);
         }
