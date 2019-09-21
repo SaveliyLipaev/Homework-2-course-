944c5e959c5ce5c7e637d07d00751447a6cc5b9d
SaveliyLipaev <sava.lipaev@mail.ru>
2019-09-21 17:35:12 +0300

    added unit tests

---
+++ MyLazy/MyLazyTests/MySimpleLazyTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MyLazy;
+
+namespace MyLazyTests
+{
+    [TestClass]
+    public class MySimpleLazyTests
+    {
+        private MySimpleLazy<int> simpleLazy; 
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            simpleLazy = LazyFactory<int>.CreateSimpleLazy(() =>
+            {
+                int result = 0;
+                for (var i = 0; i < 100; i++)
+                {
+                    result += i;
+                }
+                return result;
+            });
+        }
+
+        [TestMethod]
+        public void GetTests()
+        {
+            Assert.AreEqual(4950, simpleLazy.Get());
+        }
+    }
+}
