ae6aca56d9c1c19aee2671389a93b6515074e849
SaveliyLipaev <sava.lipaev@mail.ru>
2019-09-21 16:35:32 +0300

    lock in the right place and comments added

--- MyLazy/MyLazy/MyMultithreadedLazy.cs
+++ MyLazy/MyLazy/MyMultithreadedLazy.cs
@@ -1,14 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MyLazy
 {
+    /// <summary>
+    /// Мультипопоточная версия Lazy с гарантией корректной работы в многопоточном режиме
+    /// </summary>
     public class MyMultithreadedLazy<T> : ILazy<T>
     {
+        /// <summary>
+        /// Переменная, хранящая результат вычисления
+        /// </summary>
         private T result;
+
+        /// <summary>
+        /// Объект, предоставляющий вычисление
+        /// </summary>
         private Func<T> supplier;
+
+        /// <summary>
+        /// Флаг, если вычисление уже произошло, то true
+        /// </summary>
         private bool counted = false;
+
         private object locker = new object();
 
         public MyMultithreadedLazy(Func<T> supplier)
@@ -16,19 +29,26 @@ namespace MyLazy
             this.supplier = supplier;
         }
 
-        public T Get()  
+        /// <summary>
+        /// Возвращает вычисленное значение
+        /// </summary>
+        public T Get()
         {
-            lock (locker)
+            if (!counted)
             {
-                if (counted)
+                lock (locker)
                 {
-                    return result;
-                }
-                else
-                {
+                    if (counted)
+                    {
+                        return result;
+                    }
                     counted = true;
                     return result = supplier();
                 }
+            }
+            else
+            {
+                return result;
             }
         }
     }
