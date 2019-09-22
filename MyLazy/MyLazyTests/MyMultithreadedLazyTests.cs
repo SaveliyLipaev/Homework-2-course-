801a855044c0322241a333410eb1726eb0b8b777
SaveliyLipaev <sava.lipaev@mail.ru>
2019-09-22 16:03:21 +0300

    Added not bad tests

--- MyLazy/MyLazyTests/MyMultithreadedLazyTests.cs
+++ MyLazy/MyLazyTests/MyMultithreadedLazyTests.cs
@@ -1,32 +1,73 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using MyLazy;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace MyLazyTests
 {
     [TestClass]
     public class MyMultithreadedLazyTests
     {
-        private MySimpleLazy<int> simpleLazy;
+        private MyMultithreadedLazy<int> lazy;
+        private List<Thread> threadArr;
+        private int result = 0;
 
         [TestInitialize]
         public void Initialize()
         {
-            simpleLazy = LazyFactory<int>.CreateSimpleLazy(() =>
+            lazy = LazyFactory<int>.CreateMymultithreadedLazy(() =>
             {
-                int result = 0;
-                for (var i = 0; i < 100; i++)
+                for (var i = 0; i < 10; i++)
                 {
                     result += i;
                 }
                 return result;
             });
+
+            threadArr = new List<Thread>();
+            for (int i = 0; i < 5; i++)
+            {
+                Thread devthread = new Thread(() => lazy.Get());
+                threadArr.Add(devthread);
+            }
         }
 
         [TestMethod]
         public void GetTests()
         {
-            Assert.AreEqual(4950, simpleLazy.Get());
+
+            foreach (var thr in threadArr)
+            {
+                thr.Start();
+            }
+            foreach (var thr in threadArr)
+            {
+                thr.Join();
+            }
+            Assert.AreEqual(45, result);
+        }
+
+        [TestMethod]
+        public void GetTimeSecondsCallTests()
+        {
+            var myStopwatchSecondGet = new System.Diagnostics.Stopwatch();
+            threadArr[0].Start();
+
+            myStopwatchSecondGet.Start();
+            for (var i = 1; i < 5; i++)
+            {
+                threadArr[i].Start();
+            }
+            foreach (var thr in threadArr)
+            {
+                thr.Join();
+            }
+            myStopwatchSecondGet.Stop();
+
+            double oldTime = myStopwatchSecondGet.Elapsed.Seconds + (double)myStopwatchSecondGet.Elapsed.Milliseconds / 60;
+
+            Assert.IsTrue(oldTime < 0.00001);
         }
     }
 }
--- MyLazy/MyLazyTests/MySimpleLazyTests.cs
+++ MyLazy/MyLazyTests/MySimpleLazyTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using MyLazy;
+using System.Threading;
 
 namespace MyLazyTests
 {
@@ -15,9 +16,10 @@ namespace MyLazyTests
             simpleLazy = LazyFactory<int>.CreateSimpleLazy(() =>
             {
                 int result = 0;
-                for (var i = 0; i < 100; i++)
+                for (var i = 0; i < 10; i++)
                 {
                     result += i;
+                    Thread.Sleep(10);
                 }
                 return result;
             });
@@ -26,7 +28,31 @@ namespace MyLazyTests
         [TestMethod]
         public void GetTests()
         {
-            Assert.AreEqual(4950, simpleLazy.Get());
+            Assert.AreEqual(45, simpleLazy.Get());
         }
+
+        [TestMethod]
+        public void GetTimeSecondCallTests()
+        {
+            var myStopwatchSecondGet = new System.Diagnostics.Stopwatch();
+            simpleLazy.Get();
+
+            myStopwatchSecondGet.Start();
+            simpleLazy.Get();
+            myStopwatchSecondGet.Stop();
+
+            double oldTime = myStopwatchSecondGet.Elapsed.Seconds + (double)myStopwatchSecondGet.Elapsed.Milliseconds / 60;
+
+            Assert.IsTrue(oldTime < 0.00001);
+        }
+
+        [TestMethod]
+        public void GetReturnNull()
+        {
+            var lazy = LazyFactory<object>.CreateSimpleLazy(() => null);
+            Assert.IsNull(lazy.Get());
+        }
+
+
     }
 }
