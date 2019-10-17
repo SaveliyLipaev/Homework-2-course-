49942d2d9479d960c9d3b2acf900676bd0a8c715
SaveliyLipaev <sava.lipaev@mail.ru>
2019-10-17 17:17:22 +0300

    Delete myTask

--- MyThreadPool/MyThreadPool/MyThreadPool.cs
+++ MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -39,7 +39,7 @@ namespace MyThreadPool
             }
             catch
             {
-                throw new Exception("Thread pool has been shutted down");
+                throw new InvalidOperationException("Thread pool has been shutted down");
             }
 
             return task;
@@ -74,6 +74,61 @@ namespace MyThreadPool
                         queueTask?.Take().Invoke();
                     }
                 }).Start();
+            }
+        }
+
+        private class MyTask<TResult> : IMyTask<TResult>
+        {
+            private MyThreadPool threadPool;
+            private Func<TResult> function;
+            private ManualResetEvent waitHandler = new ManualResetEvent(false);
+            private AggregateException exception;
+            private TResult result;
+
+            public bool IsComleted { get; private set; } = false;
+
+            public TResult Result
+            {
+                get
+                {
+                    waitHandler.WaitOne();
+                    if (exception == null)
+                    {
+                        return result;
+                    }
+
+                    throw exception;
+                }
+            }
+
+            public MyTask(Func<TResult> task, MyThreadPool threadPool)
+            {
+                function = task;
+                this.threadPool = threadPool;
+            }
+
+            /// <summary>
+            /// Add new task based on result of this task.
+            /// </summary>
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func) => threadPool.AddTask(() => func(Result));
+
+            /// <summary>
+            /// Calculate task and assings Result and IsCompleted properties.
+            /// </summary>
+            public void Calculate()
+            {
+                try
+                {
+                    result = function();    
+                }
+                catch (Exception ex)
+                {
+                    exception = new AggregateException(ex);
+                }
+
+                IsComleted = true;
+                function = null;
+                waitHandler.Set();
             }
         }
     }
