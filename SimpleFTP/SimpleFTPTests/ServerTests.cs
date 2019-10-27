beef2321f8a43d8abb23704c362972999165860f
SaveliyLipaev <sava.lipaev@mail.ru>
2019-10-28 02:41:16 +0300

    Added tests

---
+++ SimpleFTP/SimpleFTPTests/ServerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleFTP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFTP.Tests
+{
+    [TestClass()]
+    public class ServerTests
+    {
+        private Server server;
+        private Client client;
+        private string path = "../../../../SimpleFTPTests/TestFiles";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            server = new Server(1333);
+            server.Start();
+            client = new Client("localhost", 1333);
+        }
+
+        [TestMethod]
+        public async Task ListCommandTestAsync()
+        {
+            var answer = await client.ListCommand(path);
+            Assert.AreEqual("2 TestFile.txt false TestDir True ", answer);
+            server.Stop();
+            client.Close();
+        }
+
+        [TestMethod]
+        public async Task ListCommandNotRightPath()
+        {
+            var answer = await client.ListCommand(path + "/NotExist");
+            Assert.AreEqual("-1", answer);
+            server.Stop();
+            client.Close();
+        }
+
+        [TestMethod]
+        public async Task GetCommandTestGoodSize()
+        {
+            var (size, content) = await client.GetCommand(path + "/TestFile.txt");
+            Assert.AreEqual(24, size);
+            server.Stop();
+            client.Close();
+        }
+
+        [TestMethod]
+        public async Task GetCommandTestGoodContent()
+        {
+            var (size, content) = await client.GetCommand(path + "/TestFile.txt");
+            Assert.AreEqual("EF-BB-BF-46-69-6C-65-20-66-6F-72-20-74-65-73-74-20-70-72-6F-6A-65-63-74", BitConverter.ToString(content));
+            server.Stop();
+            client.Close();
+        }
+
+        [TestMethod]
+        public async Task GetCommandBadPath()
+        {
+            var (size, content) = await client.GetCommand(path + "/TestFileeeeeee.txt");
+            Assert.AreEqual(-1, size);
+            server.Stop();
+            client.Close();
+        }
+    }
+}
