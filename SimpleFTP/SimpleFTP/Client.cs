12508eb5f5b05c91a1e634a8173e0efe9deb56ae
SaveliyLipaev <sava.lipaev@mail.ru>
2019-10-27 21:40:37 +0300

    Рабочий каркас

---
+++ SimpleFTP/SimpleFTP/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFTP
+{
+    class Client
+    {
+        private TcpClient client;
+        private StreamWriter writer;
+        private StreamReader reader;
+
+        public Client(string host, int port)
+        {
+            client = new TcpClient(host, port);
+            var stream = client.GetStream();
+
+            writer = new StreamWriter(stream) { AutoFlush = true };
+
+            reader = new StreamReader(stream);
+        }
+
+        public async Task<string> ListCommand(string path)
+        {
+            await writer.WriteLineAsync("1 " + path);
+            return await reader.ReadLineAsync();
+        }
+
+        public async Task<(long, byte[])> GetCommand(string path)
+        {
+            await writer.WriteLineAsync("2 " + path);
+            var size = long.Parse(await reader.ReadLineAsync());
+
+            var content = new byte[size];
+            await reader.BaseStream.ReadAsync(content);
+
+            return (size, content);
+        }
+    }
+}
---
+++ SimpleFTP/SimpleFTP/Server.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Linq;
+
+namespace SimpleFTP
+{
+    class Server
+    {
+        private TcpListener listener;
+        private CancellationTokenSource stopToken = new CancellationTokenSource();
+
+        public Server(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+        
+        public async Task Start()
+        {
+            listener.Start();
+
+            try
+            {
+                while (!stopToken.IsCancellationRequested)
+                {
+                    var client = await listener.AcceptTcpClientAsync();
+
+                    await Task.Run(() => ServiceClient(client));
+                }
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+
+        private async Task ServiceClient(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                var writer = new StreamWriter(stream) { AutoFlush = true };
+                var reader = new StreamReader(stream);
+
+                while (!stopToken.IsCancellationRequested)
+                {
+                    var message = await reader.ReadLineAsync();
+
+                    var (command, path) = ParseMessage(message);
+
+                    switch(command)
+                    {
+                        case "1":
+                            await writer.WriteLineAsync(List(path));
+                            break;
+
+                        case "2":
+                            var contentStream = File.OpenRead(path);
+                            await writer.WriteLineAsync(contentStream.Length.ToString());
+                            contentStream.CopyTo(writer.BaseStream);
+                            contentStream.Close();
+                            break;
+
+                        default:
+                            await writer.WriteLineAsync("Command is not found.");
+                            break;
+                    }
+
+                }
+            } 
+        }
+
+        private (string, string) ParseMessage(string str)
+        {
+            var tempStr = str.Split();
+            return (tempStr[0],tempStr[1]);
+        }
+
+        private string List(string path)
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(path);
+                var filesName = directoryInfo.GetFiles();
+                var directorysName = directoryInfo.GetDirectories();
+
+                return filesName.Length + directorysName.Length + " "
+                    + string.Join("", filesName.Select(name => $"{name.Name} false "))
+                    + string.Join("", directorysName.Select(name => $"{name.Name} True "));
+            }
+            catch
+            {
+                return "-1";
+            }
+        }
+    }
+}
