189a0d9154e0d7a3353fd1d5f9cd055f830db0e0
SaveliyLipaev <sava.lipaev@mail.ru>
2019-10-29 16:41:05 +0300

    Попытка обработать ошибки сети

--- SimpleFTP/SimpleFTP/Client.cs
+++ SimpleFTP/SimpleFTP/Client.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Net.Sockets;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace SimpleFTP
@@ -14,34 +13,92 @@ namespace SimpleFTP
         private TcpClient client;
         private StreamWriter writer;
         private StreamReader reader;
+        private string host;
+        private int port;
 
-        public bool IsConnection { get; private set; } = false;
+        public bool IsConnect { get; private set; } = false;
 
         public Client(string host, int port)
         {
-            try
+            client = new TcpClient();
+            this.host = host;
+            this.port = port;
+        }
+
+        /// <summary>
+        /// Client connection to server
+        /// </summary>
+        private async Task<bool> Connect()
+        {
+            var delay = TimeSpan.FromSeconds(1);
+            int counter = 0;
+            for (var i = 0; i < 3; ++i)
             {
-                client = new TcpClient(host, port);
-            }
-            catch
-            {
-                for (var i = 0; i < 4 && client == null; ++i)
+                try
                 {
-                    client = new TcpClient(host, port);
-                    Thread.Sleep(20);
+                    client.Connect(host, port);
+                    break;
                 }
+                catch 
+                {
+                    ++counter;
+                }
+                await Task.Delay(delay);
+                delay *= 2;
             }
 
-            if (client != null)
+            if (counter != 3)
             {
                 var stream = client.GetStream();
-
                 writer = new StreamWriter(stream) { AutoFlush = true };
-
                 reader = new StreamReader(stream);
-
-                IsConnection = true;
+                return IsConnect = true;
             }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Method for sending a message to the server, with repeated requests 
+        /// if a network error occurs
+        /// </summary>
+        private async Task WriteMessage(string message)
+        {
+            var delay = TimeSpan.FromSeconds(1);
+            for (var i = 0; i < 3; ++i)
+            {
+                try
+                {
+                    await writer.WriteLineAsync(message);
+                    return;
+                }
+                catch { }
+                await Task.Delay(delay);
+                delay *= 2;
+            }
+
+            await writer.WriteLineAsync(message);
+        }
+
+        /// <summary>
+        /// Method for receiving messages from the server, 
+        /// with repeated requests if a network error occurs
+        /// </summary>
+        private async Task<string> ReadMessage()
+        {
+            var delay = TimeSpan.FromSeconds(1);
+            for (var i = 0; i < 3; ++i)
+            {
+                try
+                {
+                    return await reader.ReadLineAsync();
+                }
+                catch { }
+                await Task.Delay(delay);
+                delay *= 2;
+            }
+
+            return await reader.ReadLineAsync();
         }
 
         /// <summary>
@@ -57,8 +114,20 @@ namespace SimpleFTP
         /// </summary>
         public async Task<string> ListCommand(string path)
         {
-            await writer.WriteLineAsync("1 " + path);
-            return await reader.ReadLineAsync();
+            if (!IsConnect)
+            {
+                throw new InvalidOperationException();
+            }
+
+            try
+            {
+                await WriteMessage("1 " + path);
+                return await ReadMessage();
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         /// <summary>
@@ -66,19 +135,31 @@ namespace SimpleFTP
         /// </summary>
         public async Task<(long, byte[])> GetCommand(string path)
         {
-            await writer.WriteLineAsync("2 " + path);
-
-            var size = long.Parse(await reader.ReadLineAsync());
-
-            if (size == -1)
+            if (!IsConnect)
             {
-                return (-1, null);
+                throw new InvalidOperationException();
             }
-  
-            var content = new byte[size];
-            await reader.BaseStream.ReadAsync(content);
 
-            return (size, content);
+            try
+            {
+                await WriteMessage("2 " + path); 
+
+                var size = long.Parse(await ReadMessage());
+
+                if (size == -1)
+                {
+                    return (-1, null);
+                }
+
+                var content = new byte[size];
+                await reader.BaseStream.ReadAsync(content);
+
+                return (size, content);
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
