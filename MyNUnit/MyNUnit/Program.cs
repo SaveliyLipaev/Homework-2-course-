2cb3a6aa965e6dd09bf6845eb1880ce6b1e9fa18
SaveliyLipaev <sava.lipaev@mail.ru>
2019-12-04 17:44:59 +0300

    Added app part

--- MyNUnit/MyNUnit/Program.cs
+++ MyNUnit/MyNUnit/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace MyNUnit
+﻿using System;
+using System.IO;
+
+namespace MyNUnit
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nothing is entered into the application");
+                return;
+            }
 
+            var path = args[0];
+
+            try
+            {
+                Console.WriteLine("Test execution has begun\n");
+                MyNUnitRunner.Run(path);
+                Console.WriteLine("Test execution is over\n");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Error: Directory not found");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error: The path is the file name or something else");
+            }
+            catch (AggregateException exception)
+            {
+                var invalidOperationExcaption = exception.InnerException?.InnerException;
+                if (invalidOperationExcaption.GetType() == typeof(InvalidOperationException))
+                {
+                    Console.WriteLine($"Error occured: {invalidOperationExcaption.Message}");
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 }
