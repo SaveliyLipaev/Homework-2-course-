e600a3270e5bbca01d47c3be9a8dcc7aa3499834
Saveliy <sava.lipaev@mail.ru>
2020-11-16 04:27:54 +0300

    Add 4 homework

---
+++ FirstStepsInF#/Homework_4_GenricsInFSharp/Task1/BracketChecker.fs
@@ -0,0 +1,52 @@
+﻿module Homework4.BracketChecker
+
+// Условие задачи:
+//    Реализовать функцию, которая по произвольной строке
+//    проверяет корректность скобочной последовательности в этой строке.
+//    Скобки бывают трёх видов.
+
+/// Structure represent bracket pair
+type Bracket =
+    struct
+        val opening: char
+        val closing: char
+        new(op: char, cl: char) = { opening = op; closing = cl }
+    end
+
+/// Creates matching brackets from defined symbols
+let makeBracket bracket =
+    match bracket with
+    | ')' | '(' -> Some(Bracket('(', ')'))
+    | '>' | '<' -> Some(Bracket('<', '>'))
+    | '}' | '{' -> Some(Bracket('{', '}'))
+    | _ -> None
+
+/// Checks if string is balanced
+let checkBalance (s: string) =
+    let rec balance chars buffer =
+        match chars with
+        | [] -> List.empty = buffer
+        | head :: tail ->
+            match makeBracket head with
+            | None -> balance tail buffer
+            | Some (x) when head = x.opening -> balance tail (head :: buffer)
+            | Some (x) -> if buffer.Head = x.opening then balance tail buffer.Tail else false
+
+    balance (s.ToCharArray() |> List.ofArray) []
+
+
+[<EntryPoint>]
+let main _ =
+    printfn "_____________"
+    printfn "[Simple test]\n"
+
+    printfn
+        "\"%s\" is balanced: %b"
+        "([(other < |symbols > do not matter )])"
+        ("([(hidden < |symbols > do not matter )])" |> checkBalance)
+
+    printfn
+        "\"%s\" is balanced: %b"
+        "(<(  ͡• ͜ʖ ͡• )--<)"
+        ("(<(  ͡• ͜ʖ ͡• )--<)" |> checkBalance)
+    0
---
+++ FirstStepsInF#/Homework_4_GenricsInFSharp/Task3/Phonebook.fs
@@ -0,0 +1,128 @@
+﻿module Homework4.Phonebook
+
+open System
+open System.IO
+open System.Runtime.Serialization.Formatters.Binary
+
+// Условие задачи:
+// Написать программу - телефонный справочник.
+// Она должна уметь хранить имена и номера телефонов,
+// в интерактивном режиме осуществлять следующие операции:
+//    1. выйти
+//    2. добавить запись (имя и телефон)
+//    3. найти телефон по имени
+//    4. найти имя по телефону
+//    5. вывести всё текущее содержимое базы
+//    6. сохранить текущие данные в файл
+//    7. считать данные из файла
+
+/// Adds new contact to DB
+let addToDB phone name listAsDB = (phone, name) :: listAsDB
+
+/// Finds by phone number
+let findNameByPhone phone listAsDB =
+    listAsDB
+    |> List.filter (fun (current, _) -> current = phone)
+    |> List.map (snd)
+
+/// Finds by contact name
+let findPhoneByName name listAsDB =
+    listAsDB
+    |> List.filter (fun (_, current) -> current = name)
+    |> List.map (fst)
+
+/// Prints data from given DB (list plays DB role)
+let rec printData listAsDB =
+    match listAsDB with
+    | [] -> printfn "%s" "_"
+    | h :: tail ->
+        printfn "%A" h
+        printData tail
+
+/// Saves DB to a DAT file
+let saveToFile listAsDB =
+    let formatter = BinaryFormatter()
+
+    use stream =
+        new FileStream("DB.dat", FileMode.Create)
+
+    formatter.Serialize(stream, box listAsDB)
+
+/// Reads DB from a DAT file by given filename
+let readFromFile filename =
+    let formatter = BinaryFormatter()
+    try
+        use stream = new FileStream(filename, FileMode.Open)
+        let dat = formatter.Deserialize(stream)
+        match dat with
+        | :? ((string * string) list) as l -> l
+        | _ -> raise (FormatException("Could not parse data; incorrect format"))
+
+    with
+    | :? FormatException ->
+        raise (Exception("Error error occurred during file read; unexpected format or wrong filename"))
+    | :? FileNotFoundException -> raise (ArgumentException(sprintf "Could not find file with this name: %s" filename))
+
+/// Prints options to console
+let printInfo =
+    printfn "Choose what to do (type operation number):\n"
+    printfn "\t1) Exit"
+    printfn "\t2) Add new contact"
+    printfn "\t3) Find name by phone"
+    printfn "\t4) Find phone by name"
+    printfn "\t5) Print all"
+    printfn "\t6) Save to file"
+    printfn "\t7) Read from file"
+
+/// Activates interactive mode
+let rec interactiveInterface listAsDB =
+    printInfo
+    let command = Console.ReadLine()
+    match command with
+    | "1" -> ignore
+    | "2" ->
+        printfn "%s" "type phone & name"
+        let phone = Console.ReadLine()
+        let name = Console.ReadLine()
+        printfn "%s" "adding.."
+        interactiveInterface (addToDB phone name listAsDB)
+    | "3" ->
+        printfn "%s" "type phone"
+        let phone = Console.ReadLine()
+        printfn "%A" (findNameByPhone phone listAsDB)
+        interactiveInterface listAsDB
+    | "4" ->
+        printfn "%s" "type name"
+        let name = Console.ReadLine()
+        printfn "%A" (findPhoneByName name listAsDB)
+        interactiveInterface listAsDB
+    | "5" ->
+        printData listAsDB
+        interactiveInterface listAsDB
+    | "6" ->
+        saveToFile listAsDB
+        printfn "%s" "saving..."
+        interactiveInterface listAsDB
+    | "7" ->
+        printfn "%s" "type file name to load"
+        let filename = Console.ReadLine()
+        try
+            let fileData = readFromFile filename
+            printfn "%s" "successful"
+            interactiveInterface fileData
+        with :? ArgumentException as e ->
+            printfn "%s" e.Message
+            interactiveInterface listAsDB
+    | _ ->
+        printfn "%s" "Incorrect command try again"
+        interactiveInterface listAsDB
+
+[<EntryPoint>]
+let main _ =
+    let testDB =
+        [ ("8800", "Ilya")
+          ("8800", "Dima")
+          ("5535", "Mom") ]
+
+    interactiveInterface testDB |> ignore
+    0
